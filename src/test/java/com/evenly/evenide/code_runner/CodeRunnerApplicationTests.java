cd56ee7e518d86e495ff2bf07648cf6955119c29
nahyun <knahyuk@gmail.com>
2025-04-14 21:58:57 +0900

    init: 프로젝트 초기 셋팅

---
+++ src/test/java/com/evenly/evenide/code_runner/CodeRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenly.evenide.code_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
